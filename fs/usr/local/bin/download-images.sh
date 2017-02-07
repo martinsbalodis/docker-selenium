fe905eef8545edf11515b47af6798217c992db65
Martins Balodis <martins256@gmail.com>
2017-02-07 16:42:40 +0200

    change image sizes to 800x600

--- fs/usr/local/bin/download-images.sh
+++ fs/usr/local/bin/download-images.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 mkdir -p /root/Pictures
-for i in {1..10}; do wget https://unsplash.it/500/500/?random -O "/root/Pictures/$i.jpg"; done
+for i in {1..10}; do wget https://unsplash.it/800/600/?random -O "/root/Pictures/$i.jpg"; done
